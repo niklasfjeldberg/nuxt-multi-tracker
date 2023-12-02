29f813967623a93c9881aa47216170f3934d45ea
Niklas Fjeldberg <github@fjeldberg.me>
2023-12-02 10:41:21 +0100

    fix: basic test

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,7 +1,17 @@
-import MyModule from '../../../src/module'
+import MyModule from '../../../src/module';
 
 export default defineNuxtConfig({
-  modules: [
-    MyModule
-  ]
-})
+  modules: [MyModule],
+  ssr: true,
+  multiAnalytics: {
+    private: {},
+    public: {
+      debug: true,
+      disabled: false,
+      initialConsent: false,
+      meta: {
+        pixelID: process.env.META_PIXEL_ID,
+      },
+    },
+  },
+});
