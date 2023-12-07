8c8d6c77968e9578784425efc4827cc049789c05
Niklas Fjeldberg <github@fjeldberg.me>
2023-12-07 15:02:30 +0100

    test: failed attempt to write composable test

---
+++ test/usePixelMeta.test.ts
@@ -0,0 +1,26 @@
+import { describe, it, expect } from 'vitest';
+import { fileURLToPath } from 'node:url';
+import { setup } from '@nuxt/test-utils';
+/* import usePixelMeta from 'my-module'; */
+
+describe('usePixelMeta', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
+  });
+
+  it('Event queue works', async () => {
+    /* const pixel = usePixelMeta();
+    const { options } = pixel;
+    options.isEnabled = true;
+    // options.pixelID = 'test';
+
+    expect(options.eventsQueue).toHaveLength(0);
+    pixel.track();
+    expect(options.eventsQueue).toHaveLength(1);
+    pixel.track('Lead');
+    expect(options.eventsQueue).toHaveLength(2);
+ */
+
+    expect(true).toBe(true);
+  });
+});
